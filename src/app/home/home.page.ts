26cc8e21bcf55f9fe9ca5347bbbb61adab7ab6c7
joshdlcruz <40876758+joshdlcruz@users.noreply.github.com>
2019-07-22 14:58:41 +0800

    HTTP in Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  users: any[];
 
-  constructor() {}
-
+  constructor(public http: Http) {
+    this.getData();
+  }
+  getData() {
+    this.http.get('http://jsonplaceholder.typicode.com/users').subscribe(
+      data => {
+        console.log(data.json());
+        this.users = data.json();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
