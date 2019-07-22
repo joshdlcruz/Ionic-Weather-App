448582859c4930f192e8c95cf9ad681a3a14a895
joshdlcruz <40876758+joshdlcruz@users.noreply.github.com>
2019-07-22 20:41:22 +0800

    Finishing the app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,19 +9,31 @@ import { Http } from '@angular/http';
 })
 export class HomePage {
   weatherInfo: any = {};
+  cityName = 'London';
 
-  constructor(public http: Http) {
+  constructor(public http: Http, public loadingCtrl: LoadingController) {
     this.getData();
   }
-  getData() {
-    this.http.get('http://api.openweathermap.org/data/2.5/weather?q=london&id=524901&APPID=3c9b6353b98f4955c12a6286f191da79').subscribe(
-      data => {
-        console.log(data.json());
-        this.weatherInfo = data.json();
-      },
-      error => {
-        console.log(error);
-      }
-    );
+  async getData() {
+    let loading = await this.loadingCtrl.create();
+    loading.present();
+    
+    this.http
+      .get(
+        'http://api.openweathermap.org/data/2.5/weather?q=' +
+          this.cityName +
+          '&id=524901&APPID=3c9b6353b98f4955c12a6286f191da79'
+      )
+      .subscribe(
+        data => {
+          console.log(data.json());
+          this.weatherInfo = data.json();
+          loading.dismiss();
+        },
+        error => {
+          console.log(error);
+          loading.dismiss();
+        }
+      );
   }
 }
